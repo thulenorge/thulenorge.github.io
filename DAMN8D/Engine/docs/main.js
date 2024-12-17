f49c33ad554112ea52b869a1a189e422c0b7cd39
thulenorge <thulenorge@gmail.com>
2024-12-17 14:19:22 +0300

    Update main.js

--- DAMN8D/Engine/docs/main.js
+++ DAMN8D/Engine/docs/main.js
@@ -2,10 +2,14 @@ function load(url) {
 	fetch(url).then(function (response) {
 		return response.text();
 	}).then(function (data) {
-		document.querySelector("#Markdown").innerHTML = data;
+		document.querySelector("#Markdown").innerHTML = markdown(data);
 	}).catch(function (error) {
 		documnt.querySelector("#Markdown").innerText = "Page not found!";
 	});
+}
+
+function markdown(data) {
+	return data;
 }
 
 (function main() {
