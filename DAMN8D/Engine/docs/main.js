fcc76daf35b603a8980acc33860ae07e84892598
thulenorge <thulenorge@gmail.com>
2024-12-17 11:59:39 +0300

    Update main.js

--- DAMN8D/Engine/docs/main.js
+++ DAMN8D/Engine/docs/main.js
@@ -1,14 +1,22 @@
 async function loadText(url) {
-	var response = await fetch(url);
-	return await response.text();
+	var xhr = new XMLHttpRequest();
+
+	xhr.open("GET", url, false);
+	xhr.onload = function (e) {
+		data = this.responseText;
+	};
+
+	xhr.send();
+
+	return xhr;
 }
 
-async function markdown(data) {
+function markdown(data) {
 	md = md.replace(/[#]{1}(.+)/g, "<h1>$1</h1>");
 
 	return data;
 }
 
-(async function main() {
-	document.body.innerHTML = markdown(await loadText("v0.0.0.0_a.md"));
+(function main() {
+	document.body.innerHTML = markdown(loadText("v0.0.0.0_a.md"));
 })();
