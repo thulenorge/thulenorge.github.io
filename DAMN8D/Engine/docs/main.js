218e5e0aa9541fb7b5327243a785f00185ae565e
thulenorge <thulenorge@gmail.com>
2024-12-17 14:28:46 +0300

    Update main.js

--- DAMN8D/Engine/docs/main.js
+++ DAMN8D/Engine/docs/main.js
@@ -9,9 +9,18 @@ function load(url) {
 }
 
 function markdown(data) {
+	data = data.replace(/[#]{6}(.+)/g, "<h6>$1</h6>");
+	data = data.replace(/[#]{5}(.+)/g, "<h5>$1</h5>");
+	data = data.replace(/[#]{4}(.+)/g, "<h4>$1</h4>");
+	data = data.replace(/[#]{3}(.+)/g, "<h3>$1</h3>");
+	data = data.replace(/[#]{2}(.+)/g, "<h2>$1</h2>");
+	data = data.replace(/[#]{1}(.+)/g, "<h1>$1</h1>");
+	
 	return data;
 }
 
 (function main() {
-	load(document.querySelector("select").value);
+	document.querySelector("selector").onchange = function (e) {
+		load(e.options[e.selectedIndex].text);
+	});
 })();
